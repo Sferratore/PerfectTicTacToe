045fc37fdba36dcac332a03567ae8d7a2880585a
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-01-18 12:57:50 +0100

    First commit: application lets you insert your X and computer calculates his O. Need to add check for victory, handling wrong user input in toto, diagonalcheck for CPU.

---
+++ PerfectTicTacToe/Program.cs
@@ -0,0 +1,294 @@
+﻿using System;
+
+namespace PerfectTicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] table = new int[3, 3];
+            string input;
+
+            showTable(table);   //Showing the empty table at first
+
+            Console.WriteLine();
+            Console.Write("Hi! Welcome to PerfectTicTacToe. Would you like to go first? (Y/N) >> ");
+
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Y":
+                    addX(table);
+                    break;
+
+                case "N":
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong input! :(");
+                    return;
+                    break;
+            }
+
+
+            while (true)
+            {
+                Console.Clear();
+                addO(table);
+                showTable(table);
+                addX(table);
+                Console.Clear();
+                showTable(table);
+            }
+
+
+        }
+
+
+        static void addX(int[,] table)
+        {
+            int x, y;
+            Console.Write("What is the position of your next move? (horizontally, left = 0 and right = 2) >> ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("What is the position of your next move? (vertically, bottom = 0 and top = 2) >> ");
+            y = int.Parse(Console.ReadLine());
+
+            table[x, y] = 1;
+        }
+
+        static void addO(int[,] table)
+        {
+
+            int flagAdded = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (checkHorizontal(table, i))
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (table[j, i] == 0)
+                        {
+                            table[j, i] = 2;
+                            flagAdded++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (flagAdded == 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (checkVertical(table, i))
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (table[i, j] == 0)
+                        {
+                            table[i, j] = 2;
+                            flagAdded++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (flagAdded == 1)
+            {
+                return;
+            }
+
+            randPos(table);
+
+        }
+
+        static bool checkHorizontal(int[,] table, int row)
+        {
+            int xnum = 0;
+            int onum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (table[i, row] == 1)
+                {
+                    xnum++;
+                }
+                if (table[i, row] == 2)
+                {
+                    onum++;
+                }
+            }
+
+            if (xnum == 2 && onum == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool checkVertical(int[,] table, int column)
+        {
+            int xnum = 0;
+            int onum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (table[column, i] == 1)
+                {
+                    xnum++;
+                }
+                if (table[column, i] == 2)
+                {
+                    onum++;
+                }
+            }
+
+            if (xnum == 2 && onum == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void randPos(int[,] table)
+        {
+            int[] coord = new int[2];
+            Random random = new Random();
+
+            coord[0] = random.Next(3);
+            coord[1] = random.Next(3);
+            while (table[coord[0], coord[1]] != 0)
+            {
+                coord[0] = random.Next(3);
+                coord[1] = random.Next(3);
+
+            }
+
+            table[coord[0], coord[1]] = 2;
+        }
+
+        static void showTable(int[,] table)
+        {
+            switch (table[0, 2])
+            {
+                case 1:
+                    Console.Write("X |");
+                    break;
+                case 2:
+                    Console.Write("O |");
+                    break;
+                default:
+                    Console.Write("  |");
+                    break;
+            }
+            switch (table[1, 2])
+            {
+                case 1:
+                    Console.Write("  X |");
+                    break;
+                case 2:
+                    Console.Write("  O |");
+                    break;
+                default:
+                    Console.Write("    |");
+                    break;
+            }
+            switch (table[2, 2])
+            {
+                case 1:
+                    Console.WriteLine("  X  ");
+                    break;
+                case 2:
+                    Console.WriteLine("  O  ");
+                    break;
+                default:
+                    Console.WriteLine("     ");
+                    break;
+            }
+            Console.WriteLine("------------");
+
+            switch (table[0, 1])
+            {
+                case 1:
+                    Console.Write("X |");
+                    break;
+                case 2:
+                    Console.Write("O |");
+                    break;
+                default:
+                    Console.Write("  |");
+                    break;
+            }
+            switch (table[1, 1])
+            {
+                case 1:
+                    Console.Write("  X |");
+                    break;
+                case 2:
+                    Console.Write("  O |");
+                    break;
+                default:
+                    Console.Write("    |");
+                    break;
+            }
+            switch (table[2, 1])
+            {
+                case 1:
+                    Console.WriteLine("  X  ");
+                    break;
+                case 2:
+                    Console.WriteLine("  O  ");
+                    break;
+                default:
+                    Console.WriteLine("     ");
+                    break;
+            }
+
+            Console.WriteLine("------------");
+            switch (table[0, 0])
+            {
+                case 1:
+                    Console.Write("X |");
+                    break;
+                case 2:
+                    Console.Write("O |");
+                    break;
+                default:
+                    Console.Write("  |");
+                    break;
+            }
+            switch (table[1, 0])
+            {
+                case 1:
+                    Console.Write("  X |");
+                    break;
+                case 2:
+                    Console.Write("  O |");
+                    break;
+                default:
+                    Console.Write("    |");
+                    break;
+            }
+            switch (table[2, 0])
+            {
+                case 1:
+                    Console.WriteLine("  X  ");
+                    break;
+                case 2:
+                    Console.WriteLine("  O  ");
+                    break;
+                default:
+                    Console.WriteLine("     ");
+                    break;
+            }
+        }
+
+
+    }
+}
